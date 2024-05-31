1df1a561dd7eb6867145ed163e4f0cb415b35f18
Louis Choo-Choy <louis.choochoy@duke.edu>
2024-05-31 15:08:17 +0100

    added autocomplete and cleaned up

--- api.py
+++ api.py
@@ -1,14 +1,23 @@
 import requests
 
-url = "https://sky-scanner3.p.rapidapi.com/flights/search-everywhere"
+def autoCompleteCity(city):
+    # returns IATA code for city
+    url = "https://sky-scanner3.p.rapidapi.com/flights/auto-complete"
+    querystring = {"query":f"{city}"}
+    headers = {
+        "X-RapidAPI-Key": "0f40cfcaefmsh50da143d39ef59ap10f44ejsnd17e91371aa7",
+        "X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    return response.json()
 
-querystring = {"fromEntityId":"NYCA"}
 
-headers = {
-	"X-RapidAPI-Key": "0f40cfcaefmsh50da143d39ef59ap10f44ejsnd17e91371aa7",
-	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
+def findFlights(dep_IATA):
+    url = "https://sky-scanner3.p.rapidapi.com/flights/search-everywhere"
+    querystring = {"fromEntityId":f"{dep_IATA}"}
+    headers = {
+        "X-RapidAPI-Key": "0f40cfcaefmsh50da143d39ef59ap10f44ejsnd17e91371aa7",
+        "X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    print(response.json())
